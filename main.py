6e9f7aa54d45c3674fe7d2ff50c2366227d7a2bc
Alex Orear <alex.orear@gmail.com>
2017-04-05 17:16:05 -0500

    Reworked code to utilize templates and cleaned up code

--- main.py
+++ main.py
@@ -1,102 +1,26 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 import cgi
 import re
+import jinja2
+import os
 
-
-#Standard page header
-page_header = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>User Signup</title>
-    </head>
-    <body>"""
-
-#Standard page footer
-page_footer = """
-    </body>
-    </html>"""
-
+env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
+    autoescape=True)
 
 class Index(webapp2.RequestHandler):
     def get(self):
 
-        username = self.request.get('username')
-        email = self.request.get('email')
-
-        form_title = "<h2> User Signup </h2>"
-        #User input form html
-        username_label = "<label> Username </label>"
-        username_input = "<input type='text' name='username'  value='{0}'/>" .format(username)
-
-        password_label = "<label> Password </label>"
-        password_input = "<input type='password' name='password'/>"
-
-        ver_password_label = "<label> Verify Password </label>"
-        ver_password_input = "<input type='password' name='ver_password'/>"
-
-        email_label = "<label> Email (optional) </label>"
-        email_input = "<input type='text' name='email' value='{0}'/>" .format(email)
-
-        submit = "<input type='submit'/>"
-
-        # if we have an error display an error message
-        username_error = self.request.get("username_error")
-        pass_error = self.request.get("password_error")
-        email_error = self.request.get("email_error")
-
-        if username_error:
-            error_esc = cgi.escape(username_error, quote=True)
-            username_error = "<strong style='color:red'>" + error_esc + "</strong>"
-
-        if pass_error:
-            error_esc = cgi.escape(pass_error, quote=True)
-            pass_error =  "<strong style='color:red'>" + error_esc + "</strong>"
-
-        if email_error:
-            error_esc = cgi.escape(email_error, quote=True)
-            email_error =  "<strong style='color:red'>" + error_esc + "</strong>"
-
-
-
-        # form concantination
-        form = ("<form action='/Welcome' method='post'>" +
-                username_label + username_input + username_error + '<br>' + '<br>' +
-                password_label + password_input + pass_error + '<br>' + '<br>' +
-                ver_password_label + ver_password_input + '<br>' + '<br>' +
-                email_label + email_input + email_error + '<br>' + '<br>' + submit + '<br>'
-                "</form>")
-
-
-        content = page_header + form_title + form + page_footer
+        template = env.get_template('signup.html')
+        content = template.render()
         self.response.write(content)
 
-class Welcome(webapp2.RequestHandler):
     def post(self):
         # escape and define variables from user input form
-        username = cgi.escape(self.request.get("username"), quote=True)
-        password = cgi.escape(self.request.get("password"),quote=True)
-        ver_password = cgi.escape(self.request.get("ver_password"),quote=True)
-        email = cgi.escape(self.request.get("email"),quote=True)
-
-        valid_error = False
-        url_param = []
+        username = self.request.get("username")
+        password = self.request.get("password")
+        ver_password = self.request.get("ver_password")
+        email = self.request.get("email")
 
         # check for regular expressions to validate username, password and email
         user_reg = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
@@ -112,40 +36,47 @@ class Welcome(webapp2.RequestHandler):
             return email_reg.match(email)
 
         # Check for validation errors
+        valid_error = False
+        username_error = ""
+        pass_error = ""
+        email_error = ""
+
         if not valid_username(username):
-            user_error = "username_error=Please enter a valid username &username=" + username
-            #self.redirect("/?username_error=" + error + "&username=" + username)
-            url_param.append(user_error)
+            username_error = "Please enter a valid username"
             valid_error = True
-        else:
-            url_param.append("username=" + username)
 
         if not valid_password(password):
-            pass_error ="password_error=Please enter a valid password"
-            #self.redirect("/?password_error=" + error)
-            url_param.append(pass_error)
+            pass_error = "Please enter a valid password"
             valid_error = True
 
         if password != ver_password:
-            pass_error = "password_error=Passwords don't match"
-            #self.redirect("/?password_error=" + error)
-            url_param.append(pass_error)
+            pass_error = "Passwords don't match"
             valid_error = True
 
         if len(email) > 0:
             if not valid_email(email):
-                email_error = "email_error=Please enter a valid email &email=" + email
-                #self.redirect("/?email_error=" + error + "&email=" + email)
-                url_param.append(email_error)
+                email_error = "Please enter a valid email"
                 valid_error = True
 
         if valid_error == True:
-            redirect_message = ""
-            for i in url_param:
-                redirect_message += (i + "&")
-            self.redirect("/?" + redirect_message )
+        """if we have an error display an error message"""
+            template = env.get_template('signup.html')
+            content = template.render(username_error = username_error,
+                username = username, pass_error = pass_error,
+                email_error = email_error, email = email)
 
-        content = page_header + '<h2>Welcome ' + username + '!</h2>' + page_footer
+            self.response.write(content)
+
+        else:
+            self.redirect("/Welcome?username=" + username)
+
+
+class Welcome(webapp2.RequestHandler):
+    def get(self):
+        username = self.request.get("username")
+
+        template = env.get_template('welcome.html')
+        content = template.render(username = username)
         self.response.write(content)
 
 
